30f265e39853b6f2f13fa652b9910ad0d2ef07b5
Margaret <maenunezt@gmail.com>
2023-07-13 23:29:02 -0400

    ejercicio 11 finalizado

---
+++ app/src/main/java/com/example/ejercicio11/e11.kt
@@ -0,0 +1,39 @@
+package com.example.ejercicio11
+
+class e11 {
+
+    fun main() {
+        var nro1 = 10
+        val nro2 = 33
+        val nro3 = 66
+
+        var resultado = nro1 + nro2 + nro3
+
+        println("El resultado de las variables declaradas es $resultado")
+
+        nro1 = 55
+
+        resultado = nro1 + nro2 + nro3
+
+        println("El resultado de las variables declaradas es $resultado")
+
+        var promedio = resultado / 3
+
+        println("El promedio de las variables declaradas es $promedio")
+
+    }
+}
+
+fun profile() {
+    val amanda = Person("Amanda", 33, "Tennis", null)
+    val atiqah = Person("Atiqah", 28, "Climb", amanda)
+    amanda.showProfile()
+    atiqah.showProfile()
+}
+class Person (val name: String, val age: Int, val hobby: String, val referrer: Person?) {
+    fun showProfile() {
+    }
+}
+
+
+
